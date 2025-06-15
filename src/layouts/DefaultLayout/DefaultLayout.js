10508d60f4c4cd7ff9b9b23bc2cc7ef9095681df
tuandung2109 <dinhtuandung10a6@gmail.com>
2025-06-15 17:37:40 +0700

    Phân rõ các trang header sidebar content

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -11,7 +11,9 @@ function DefaultLayout({ children }) {
         <div className={cx('wrapper')}>
             <Header />
             <div className={cx('container')}>
-                <Sidebar />
+                <div className={cx('sidebar')}>
+                    <Sidebar />
+                </div>
                 <div className={cx('content')}>{children}</div>
             </div>
         </div>
